5a1c86f4cd87b716ca801c4f6943ac207e725cf5
Mira Vogt <miravogt@gmail.com>
2020-11-16 11:00:02 +0100

    refactor v2, composition & interface

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader'
 
-const reader = new MatchReader('football.csv');
-reader.read();
+//Create an Object that satisfies the Data Reader Interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (
     (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
     (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
--- src/inheritance/MatchReader.ts
+++ src/inheritance/MatchReader.ts
@@ -1,25 +1,11 @@
-import fs from "fs";
+import { CsvFileReader } from './CsvFileReader';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader {
-  data: MatchData[] = [];
-
-  constructor(public filename: string) {}
-
-
-  read(): void {
-    this.data = fs
-      .readFileSync(this.filename, {
-        encoding: "utf-8",
-      })
-      .split("\n")
-      .map((row: string): string[] => {
-        return row.split(";");
-      })
-      .map((row: string[]): MatchData => {
+export class MatchReader extends CsvFileReader<MatchData> {
+    mapRow(row: string[]): MatchData {
         return [
             dateStringToDate(row[0]),
             row[1],
@@ -29,6 +15,5 @@ export class CsvFileReader {
             row[5] as MatchResult,
             row[6].slice(0, -1)
         ]
-      })
-  }
+    }
 }
