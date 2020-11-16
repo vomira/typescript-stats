dacaf02d3dfe629120d6a78ff77ddb7d528373bb
Mira Vogt <miravogt@gmail.com>
2020-11-16 15:51:05 +0100

    finish stats project

---
+++ src/reportTargets/HtmlReport.ts
@@ -0,0 +1,16 @@
+import { OutputTarget } from '../Summary';
+import fs from 'fs';
+
+export class HtmlReport implements OutputTarget {
+    constructor(public fileName: string) {}
+
+    print(report: string): void {
+        const html = `
+        <div>
+            <h1>Analysis Output</h1>
+            <div>${report}</div>
+        </div>       
+        `;
+        fs.writeFileSync(`${this.fileName}.html`, html);
+    }
+}
